1f39d858b79b1265a6c339d3777fa3d405653b96
manish7739 <manishkumar12118@gmail.com>
2022-03-20 00:55:22 -0400

    assignment of 20 march

---
+++ index.js
@@ -0,0 +1,25 @@
+function sum(x, y){
+   let sumResult = x+ y
+    console.log("Sum : " + sumResult)
+}
+function sub(x, y){
+    let subResult = x- y
+    console.log( "sub : " + subResult)
+}
+function mul(x, y){
+    let mulResult = x*y
+    console.log("mul : " + mulResult)
+}
+function div(x, y){
+    let divResult = x/ y
+    console.log("div : " + divResult)
+}
+function hallSquare(x, y){
+    let Result = (x+y)^2
+    console.log("(x+y)^2 : "  + Result)
+}
+sum(3,4);
+sub(45,40)
+mul(3,6)
+div(24,8)
+hallSquare(3,4)
